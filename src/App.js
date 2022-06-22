dc7be3853820f23ddc497df7e498f905b4d00394
shahram02 <shahramkhaleh2001@gmail.com>
2022-06-22 05:18:15 -0700

    Reset Code and generate App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+const App = () => {
+  return (
+    <div className="App">
+      <h1>شروع پروژه تودو لیست</h1>
+      <div className="container">
+          
+      </div>
+    </div>
+  );
+};
+
+export default App;
